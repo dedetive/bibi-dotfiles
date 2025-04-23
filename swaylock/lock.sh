f9728f0e45cfb48b92ef42a96c9448dda28bbb0d
dedetive <156375166+dedetive@users.noreply.github.com>
2025-04-23 16:13:45 -0300

    Add swaylock

---
+++ swaylock/lock.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+STYLE_DIR="$HOME/.config/sway/styles"
+STYLE_MAIN="$STYLE_DIR/main.style"
+STYLE_SUCCESS="$STYLE_DIR/success.style"
+
+# Real swaylock, requires passwords
+if swaylock --config $STYLE_MAIN; then
+
+    # If following is executed, the password has already been accepted
+    swaylock --config $STYLE_SUCCESS
+
+    # Delay until it goes away
+    sleep 1.8
+
+    # Kill only the most recent swaylock daemon instance safely
+    pid=$(pgrep -n -f "swaylock.*daemonize")
+    if [ -n "$pid" ]; then
+      kill -USR1 "$pid"
+    fi
+fi
