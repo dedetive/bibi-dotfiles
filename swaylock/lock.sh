d413cd5de3cf860f76c480440c9501bcfd9bd3a2
dedetive <156375166+dedetive@users.noreply.github.com>
2025-04-24 14:42:05 -0300

    Use SCRIPT_DIR instead of path of execution

--- swaylock/lock.sh
+++ swaylock/lock.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-STYLE_DIR="./styles"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+STYLE_DIR="$SCRIPT_DIR/styles"
+echo $STYLE_DIR
 STYLE_MAIN="$STYLE_DIR/main.style"
 STYLE_SUCCESS="$STYLE_DIR/success.style"
 
